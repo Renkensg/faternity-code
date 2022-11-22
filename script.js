3e57715e7ce077deaf8f5729caa961562eb4ab35
Guilherme Renkens <renkensg@gmail.com>
2022-11-22 16:53:09 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const title = document.querySelector('h2#txtTitle')
+const descrip = document.getElementById('txtDescription')
+const welcome = document.querySelector('p#txtWelcome')
+
+title.innerHTML = `Fraternity Code`
+descrip.innerHTML = `Grupo de estudos independente. Comunidade de desenvolvedores.`
+welcome.innerHTML = `Seja bem vindo à nossa família \u{1F389}`
